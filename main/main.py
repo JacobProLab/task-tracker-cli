fb529d0b1c8103298b3c1bdd0ef689814c7f37f7
JacobProLab <jacob.provencher.science@gmail.com>
2025-03-06 11:49:01 -0500

    Implement list_task function and update README

--- main/main.py
+++ main/main.py
@@ -29,8 +29,8 @@ def add_task(filename, description):
     json_dict[id] = {
         'description': description,
         'status': 'todo',
-        'createdAt': current_time,
-        'modifiedAt': current_time
+        'created_at': current_time,
+        'modified_at': current_time
     }
 
     write_json(filename, json_dict) # Write dict to json file
@@ -48,25 +48,29 @@ def delete_task(filename, id):
     write_json(filename, json_dict) # Write dict to json file
 
 
-def mark_status(filename, id):
-    pass
-
-def list_tasks(filename):
+def mark_as_in_progress(filename, id):
 
     json_dict = read_json(filename) # Read json file content
-    table = format_as_table(json_dict)
+    json_dict[str(id)]['status'] = 'in-progress'
+    write_json(filename, json_dict) # Write dict to json file
+
+def list_tasks(filename, *filter):
+
+    json_dict = read_json(filename) # Read json file content
+    table = format_as_table(json_dict, filter)
     
     return table
 
-def format_as_table(payload):
+def format_as_table(payload, filter):
 
     table_dict = defaultdict(list)
-    for id, properties in payload.items():
-        table_dict['ID'].append(id)
-        table_dict['Description'].append(properties['description'])
-        table_dict['Status'].append(properties['status'])
-        table_dict['Created At'].append(properties['createdAt'])
-        table_dict['Modified At'].append(properties['modifiedAt'])
+    for id, properties in payload.items(): 
+        if not filter or properties['status'] in filter:
+            table_dict['ID'].append(id)
+            table_dict['Description'].append(properties['description'])
+            table_dict['Status'].append(properties['status'])
+            table_dict['Created At'].append(properties['created_at'])
+            table_dict['Modified At'].append(properties['modified_at'])
 
     return tabulate(table_dict, headers="keys", tablefmt="fancy_grid")
 
@@ -93,11 +97,15 @@ if __name__ == '__main__':
     update_parser.add_argument('description')
 
     # delete
-    delete_parser = subparsers.add_parser('delete', help='Update given task id description')
+    delete_parser = subparsers.add_parser('delete', help='Delete task according to given id')
     delete_parser.add_argument('id', type=int)
 
     # list all
-    list_all_parser = subparsers.add_parser('list', help='Return a list of all tasks')
+    list_parser = subparsers.add_parser('list', help='Return a list of all tasks')
+
+    # mark-in-progress
+    mark_in_progress_parser = subparsers.add_parser('mark-in-progress', help="Mark given task id as 'In Progress'")
+    mark_in_progress_parser.add_argument('id', type=int)
 
     # Process query
     args = parser.parse_args()
@@ -111,7 +119,8 @@ if __name__ == '__main__':
     elif args.command == 'delete':
         delete_task(filename, args.id)
 
-    elif args.command == 'list':
-        print(list_tasks(filename))
+    elif args.command == 'mark-in-progress':
+        mark_as_in_progress(filename, args.id)
+    
+    print(list_tasks(filename))
 
-# python main/main.py add "Buy new monitor"
